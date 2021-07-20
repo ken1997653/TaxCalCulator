d56ef528478b1fbfa0d187e4c921bdbce1e5221a
ken1997653 <87357239+ken1997653@users.noreply.github.com>
2021-07-20 17:57:02 +0800

    Add files via upload
    Homework Tax Calculator new Version

--- WindowsFormsApp9/WindowsFormsApp9/Form1.Designer.cs
+++ WindowsFormsApp9/WindowsFormsApp9/Form1.Designer.cs
@@ -172,12 +172,12 @@ namespace WindowsFormsApp9
 			this.label7.AutoSize = true;
 			this.label7.BackColor = System.Drawing.SystemColors.ButtonFace;
 			this.label7.Cursor = System.Windows.Forms.Cursors.WaitCursor;
-			this.label7.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Font = new System.Drawing.Font("微軟正黑體", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-			this.label7.Location = new System.Drawing.Point(219, 28);
+			this.label7.Location = new System.Drawing.Point(214, 29);
 			this.label7.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
 			this.label7.Name = "label7";
-			this.label7.Size = new System.Drawing.Size(471, 48);
+			this.label7.Size = new System.Drawing.Size(462, 50);
 			this.label7.TabIndex = 13;
 			this.label7.Text = "使用牌照稅應納稅額試算";
 			// 
